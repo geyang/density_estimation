91b6cef8f0bd3f4661cdddcd7d037b37cf8b8194
Ge Yang <yangge1987@gmail.com>
2020-12-09 17:52:40 -0800

    add bandwidth parameter

--- density_estimation/kernel_density.md
+++ density_estimation/kernel_density.md
@@ -1,11 +1,15 @@
+
+The parameter `bandwidth=0.1` is a smoothing parameter.
+
 ```python
 X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1, ], [2, 1], [3, 2]])
 
-kde = KernelDensity(kernel="gaussian", bandwidth=0.2).fit(X)
+kde = KernelDensity(kernel="gaussian", bandwidth=bandwidth)
+kde.fit(X)
 scores = kde.score_samples(X)
 doc.print(scores)
 ```
 
 ```
-[-0.41075698 -0.41075698 -0.41076071 -0.41075698 -0.41075698 -0.41076071]
+[0.97553365 0.97553365 0.97553365 0.97553365 0.97553365 0.97553365]
 ```
--- density_estimation/kernel_density.py
+++ density_estimation/kernel_density.py
@@ -2,13 +2,16 @@ from sklearn.neighbors import KernelDensity
 from cmx import doc
 import numpy as np
 
+bandwidth = 0.1
+
+doc @ f"""
+The parameter `bandwidth={bandwidth}` is a smoothing parameter.
+"""
+
 with doc:
     X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1, ], [2, 1], [3, 2]])
 
-    kde = KernelDensity(kernel="gaussian", bandwidth=0.2).fit(X)
+    kde = KernelDensity(kernel="gaussian", bandwidth=bandwidth)
+    kde.fit(X)
     scores = kde.score_samples(X)
     doc.print(scores)
-
-doc @ """
-The parameter `bandwidth` is a smoothing parameter.
-"""
